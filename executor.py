734cd6b01a1d12af84a68c3e4d793bf53e783aba
belousgm <kirill.trofimov03@mail.ru>
2021-11-22 00:40:18 +0300

    testing executor!!!

--- executor.py
+++ executor.py
@@ -4,4 +4,7 @@ import csv
 with open("anime.csv", newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row)
+        for i in row:
+            if row[i] == 'Midoriyama Koukou Koushien-hen':
+                print(row['Anime-PlanetID'])
+
